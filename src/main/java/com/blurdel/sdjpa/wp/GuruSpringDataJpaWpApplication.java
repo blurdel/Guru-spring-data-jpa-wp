28b62c00024bdbf7655689a4db1be2db27435028
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-29 17:10:59 -0400

    Demo project for Spring Data JPA WordPress - initial commit

---
+++ src/main/java/com/blurdel/sdjpa/wp/GuruSpringDataJpaWpApplication.java
@@ -0,0 +1,13 @@
+package com.blurdel.sdjpa.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruSpringDataJpaWpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruSpringDataJpaWpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blurdel/sdjpa/wp/GuruSpringDataJpaWpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blurdel.sdjpa.wp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruSpringDataJpaWpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
