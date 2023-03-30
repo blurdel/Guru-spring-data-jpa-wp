3f097a42ed2438c83ca5b60890161c5e8d991e23
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-30 14:48:28 -0400

    Added Entities and mappings for Comment and CommentMeta

--- src/main/java/com/blurdel/sdjpa/wp/domain/User.java
+++ src/main/java/com/blurdel/sdjpa/wp/domain/User.java
@@ -79,6 +79,18 @@ public class User {
     @JoinColumn(name = "user_id")
     private Set<UserMeta> userMetaSet;
 
+    @OneToMany(mappedBy = "user")
+    private Set<Comment> comments;
+
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+
     public Set<UserMeta> getUserMetaSet() {
         return userMetaSet;
     }
